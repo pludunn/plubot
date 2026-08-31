9c57874a3584c192f42015bc013b210d207c5a3c
pludunn <dunn_isaac@icloud.com>
2026-08-30 20:13:47 -0400

    Initial bot

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from plubot!")
-
-
-if __name__ == "__main__":
-    main()
+import os
+import discord
+bot=discord.Client(intents=discord.Intents.default(),status=discord.Status.offline)
+@bot.event
+async def on_ready():
+	action=input("Action: ")
+	match action:
+		case"send":
+			channel_id=int(input("Channel ID: "))
+			channel=await bot.fetch_channel(channel_id)
+			assert isinstance(channel,(discord.TextChannel,discord.VoiceChannel)),("Channel is not a text channel or a voice channel.")
+			message=input("Message: ")
+			await channel.send(message)
+		case"spam":
+			channel_id=int(input("Channel ID: "))
+			channel=await bot.fetch_channel(channel_id)
+			assert isinstance(channel,(discord.TextChannel,discord.VoiceChannel)),("Channel is not a text channel or a voice channel.")
+			message=input("Message: ")
+			amount=int(input("Amount: "))
+			for _ in range(amount):
+				await channel.send(message)
+bot.run(os.getenv("TOKEN","None"))
